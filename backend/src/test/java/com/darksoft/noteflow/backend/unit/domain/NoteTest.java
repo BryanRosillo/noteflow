174fd45b33d61ab4cec705b175c9e144ae8af50c
BryanRosillo <bryanrosillo100@hotmail.com>
2026-01-18 18:27:09 -0500

    test: create a note without title unit test added

--- backend/src/test/java/com/darksoft/noteflow/backend/unit/domain/NoteTest.java
+++ backend/src/test/java/com/darksoft/noteflow/backend/unit/domain/NoteTest.java
@@ -5,11 +5,12 @@ import org.junit.jupiter.api.Test;
 import com.darksoft.noteflow.backend.domain.entities.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class NoteTest {
 
-    @DisplayName("Create a valid note with tags")
     @Test
+    @DisplayName("Create a valid note with tags")
     public void create_a_valid_note_with_tags(){
         String title = "Do my Homework";
         String content = "I must end my math homework on typical operations.";
@@ -20,5 +21,17 @@ public class NoteTest {
         assertEquals(title, note.getTitle());
         assertEquals(content, note.getContent());
         assertEquals(tags[0].getTagName(), note.getTags()[0].getTagName());
+    }
+
+    @Test
+    @DisplayName("Create a note without title")
+    public void create_a_note_without_title(){
+        String title = "";
+        String content = "It's a note";
+        Tag[] tags = new Tag[]{new Tag("Tag01")};
+
+        var exception = assertThrows(EmptyClassException.class, () -> new Note(title, content, tags));
+
+        assertEquals("The title of the note cannot be empty.", exception.getMessage());
     }
 }
